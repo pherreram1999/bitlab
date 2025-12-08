15d3422b257771acba098b8d6dce000d4600e9d8
Pedro <alonso.pahm@gmail.com>
2025-12-07 20:25:52 -0600

    se tiene la base del layout

--- resources/js/Components/Dropdown.vue
+++ resources/js/Components/Dropdown.vue
@@ -54,15 +54,13 @@ const alignmentClasses = computed(() => {
 
         <!-- Full Screen Dropdown Overlay -->
         <div v-show="open" class="fixed inset-0 z-40" @click="open = false" />
-
         <transition
             enter-active-class="transition ease-out duration-200"
             enter-from-class="transform opacity-0 scale-95"
             enter-to-class="transform opacity-100 scale-100"
             leave-active-class="transition ease-in duration-75"
             leave-from-class="transform opacity-100 scale-100"
-            leave-to-class="transform opacity-0 scale-95"
-        >
+            leave-to-class="transform opacity-0 scale-95">
             <div
                 v-show="open"
                 class="absolute z-50 mt-2 rounded-md shadow-lg"
@@ -70,7 +68,7 @@ const alignmentClasses = computed(() => {
                 style="display: none;"
                 @click="open = false"
             >
-                <div class="rounded-md ring-1 ring-black ring-opacity-5" :class="contentClasses">
+                <div class="rounded-md " :class="contentClasses">
                     <slot name="content" />
                 </div>
             </div>
