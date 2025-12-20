418c854c97cc134577f33bac32dd33ab24d07ce2
EmiDosme <emiliozarinangomez@gmail.com>
2025-12-19 19:22:09 -0600

    Vista Home linkeada, solo falta imagen y footer.

--- routes/web/emilio.php
+++ routes/web/emilio.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AlumnoController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 //Route::inertia("/alumnos/retos", "AlumnosRetos");
 //Route::inertia('/alumnos/miembros', 'AlumnoMiembros');
@@ -9,3 +10,4 @@ Route::get('alumnos/retos',[AlumnoController::class,'verRetos'])->name('alumnos.
 Route::get('alumnos/miembros',[AlumnoController::class,'index'])->name('alumnos.miembros');
 Route::get('profesor/miembros',[\App\Http\Controllers\ProfesorController::class,'view'])->name('profesor.miembros');
 Route::get('profesor/retos',[\App\Http\Controllers\ProfesorController::class,'index'])->name('profesor.retos');
+Route::get('welcome',[WelcomeController::class,'index'])->name('welcome');
